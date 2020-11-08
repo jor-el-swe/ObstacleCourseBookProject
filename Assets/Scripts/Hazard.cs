b1fafb806a496beefd7522510be90c434b11fbc5
Joel Ring <joelwofo@users.noreply.github.com>
2020-11-08 20:24:41 +0100

    implements until shooting script p 194

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 8)
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Header("References")] public Transform trans;
+
+    [Header("Stats")] 
+    [Tooltip("How many units the projectile will move forward per second.")]
+    public float speed = 34;
+
+    [Tooltip("The distance the projectile will travel before it comes to a stop.")]
+    public float range = 70;
+
+    private Vector3 spawnPoint;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = trans.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move along z-azis
+        trans.Translate(0,0,speed *Time.deltaTime, Space.Self);
+        if (Vector3.Distance(trans.position, spawnPoint) >= range)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
