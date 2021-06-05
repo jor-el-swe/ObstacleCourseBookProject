8d97cb7834c1a55e34c0a43012563f069168446d
Joel Ring <joel.ring@gmail.com>
2021-06-05 21:18:54 +0200

    implements until "patrolling hazards" p 209

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == 8) {
+            SceneManager.LoadScene("main");
+        }
+    }
+}
